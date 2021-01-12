79efa19901bf74946f06496b5d3cae6a349b8d52
rentianshi <hbsjz_rts@163.com>
2021-01-13 00:00:30 +0800

    team2工作的第一天

---
+++ gmall-order/src/main/java/com/atguigu/gmall/oms/controller/OrderController.java
@@ -0,0 +1,10 @@
+package com.atguigu.gmall.oms.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+    public void test(){
+        System.out.println("team2工作的第一天");
+    }
+}
