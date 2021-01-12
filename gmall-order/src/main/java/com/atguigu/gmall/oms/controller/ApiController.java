417ca63c0ffd12b2c89a381eece10adf3f3162cf
rentianshi <hbsjz_rts@163.com>
2021-01-12 23:41:22 +0800

    主分支提交测试

---
+++ gmall-order/src/main/java/com/atguigu/gmall/oms/controller/ApiController.java
@@ -0,0 +1,10 @@
+package com.atguigu.gmall.oms.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ApiController {
+    public void test(){
+        System.out.println("主分支测试");
+    }
+}
